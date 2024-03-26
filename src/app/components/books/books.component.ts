80b95e3e2043548cb9d7404ce8ced38b2aa93048
Everton617 <everton6943@gmail.com>
2024-03-26 10:09:11 -0300

    Commit de conclusão da primeira feature

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA, HostListener } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-books',
   standalone: true,
   imports: [RouterOutlet,CommonModule, HttpClientModule],
   templateUrl: './books.component.html',
-  styleUrl: './books.component.scss'
+  styleUrl: './books.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class BooksComponent implements OnInit {
   public data: any;
+  slidesPerView: number = 3;
+  screenWidth!:number;
 
   constructor(private http: HttpClient){
 
@@ -19,7 +22,9 @@ export class BooksComponent implements OnInit {
 
   ngOnInit(): void {
       this.fetchDetails();
+
   }
+
 
   public fetchDetails(){
     this.http.get('http://localhost:8080/api/getAll').subscribe(
@@ -29,5 +34,19 @@ export class BooksComponent implements OnInit {
       }
     )
   }
+
+  @HostListener('window:resize')
+  getScreebWidth(){
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth >= 500 && this.screenWidth <= 800){
+      this.slidesPerView = 1;
+    }
+  }
+
+  getStarsArray(stars: number): number[] {
+    return Array(stars).fill(0).map((x, i) => i + 1);
+  }
+
+
 
 }
