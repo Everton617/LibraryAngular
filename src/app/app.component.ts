e00a8e4bff34cc1f112273bf5e015016b1eb55a8
Everton617 <everton6943@gmail.com>
2024-03-28 09:34:06 -0300

    Conclusão swiper Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BooksComponent } from './components/books/books.component';
+import { SwiperComponent } from './components/swiper/swiper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,BooksComponent],
+  imports: [RouterOutlet, HeaderComponent,BooksComponent,SwiperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/swiper/swiper.component.ts
@@ -0,0 +1,13 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swiper',
+  standalone: true,
+  imports: [],
+  templateUrl: './swiper.component.html',
+  styleUrl: './swiper.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class SwiperComponent {
+
+}
