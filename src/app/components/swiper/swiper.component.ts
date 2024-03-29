32f29bedbae1c6d6d37e6e7c8e75d5657ce1127f
Everton617 <everton6943@gmail.com>
2024-03-29 09:10:26 -0300

    Conclusão responsividade SwiperComponent

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-swiper',
@@ -9,5 +9,26 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SwiperComponent {
+  slidesPerView:number = 5;
+  screenWidth!: number;
 
+  @HostListener('window:resize')
+  getScreenWidth(){
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth >= 320 && this.screenWidth <= 500){
+      this.slidesPerView = 1;
+    }
+    else if(this.screenWidth >= 500 && this.screenWidth <= 740){
+      this.slidesPerView = 2;
+    }
+   else if(this.screenWidth >= 740 && this.screenWidth <= 970){
+      this.slidesPerView = 3;
+    }
+    else if(this.screenWidth >= 970 && this.screenWidth <= 1450){
+      this.slidesPerView = 4;
+    }
+    else if(this.screenWidth >= 1450){
+      this.slidesPerView = 6;
+    }
+  }
 }
