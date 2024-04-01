3595ae45970e8b24d7e977f056688fdf00a842f4
Everton617 <everton6943@gmail.com>
2024-04-01 10:05:13 -0300

    Conclusão do Banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BooksComponent } from './components/books/books.component';
 import { SwiperComponent } from './components/swiper/swiper.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,BooksComponent,SwiperComponent],
+  imports: [RouterOutlet, HeaderComponent,BannerComponent,BooksComponent,SwiperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
