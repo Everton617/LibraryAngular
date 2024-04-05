2ebb782dda934f04b02845c952efd19d18ac72cd
Everton617 <everton6943@gmail.com>
2024-04-05 11:48:39 -0300

    Responsividade total concluída

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule,FormsModule,MatIconModule,MatButtonModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
